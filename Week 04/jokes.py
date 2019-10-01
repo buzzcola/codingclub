b6a681f8aa7d6491ae2aa8273233707463a87e9b
James Orr <jorr@questica.com>
2019-10-01 09:36:01 -0400

    touch ups

--- Week 04/jokes.py
+++ Week 04/jokes.py
@@ -1,16 +1,24 @@
- 1. print('What do you get when you cross a snowman with a vampire?')
- 2. input()
- 3. print('Frostbite!')
- 4. print()
- 5. print('What do dentists call an astronaut\'s cavity?')
- 6. input()
- 7. print('A black hole!')
- 8. print()
- 9. print('Knock knock.')
-10. input()
-11. print("Who's there?")
-12. input()
-13. print('Interrupting cow.')
-14. input()
-15. print('Interrupting cow wh', end='')
-16. print('-MOO!')
+# This is a simple program that tells jokes. It has some
+# new things in it that we haven't seen yet though.
+
+print('What do you get when you cross a snowman with a vampire?')
+input()
+print('Frostbite!')
+print()
+
+# this string has an example of an \ escape charater. What's that for?
+# what happens if we take it out?
+print('What do dentists call an astronaut\'s cavity?')
+input()
+print('A black hole!')
+print()
+
+# this one uses a special call to the print() function. What does that do?
+print('Knock knock.')
+input()
+print("Who's there?")
+input()
+print('Interrupting cow.')
+input()
+print('Interrupting cow wh', end='')
+print('-MOO!')
